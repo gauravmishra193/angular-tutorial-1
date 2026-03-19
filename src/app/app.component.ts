2ea164519caea48823a14fbe7f6a57d24b3ae251
gaurav <gauravmishra193@gmail.com>
2026-03-19 09:45:51 +0530

    day-01

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { UserComponent } from './user/user.component';
+import { DataBindingComponent } from './data-binding/data-binding.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, DataBindingComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'Angular_19_tutorial';
+  title = 'there! Welcome to Angular_19_tutorial';
 }
